c03bc67788e4c60355dec9da91c38af03a5f1c52
Hyerim03 <chhari0708@naver.com>
2022-10-04 20:56:47 +0900

    add : fuck

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const pool = require("./config/database")
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
+app.get("/", (req,res) => {
+    res.json("hello world")
+})
+
+app.post("/",(req,res) => {
+    const {name, age} = req.body
+    pool((conn) => {
+        conn.query("INSERT INTO tbl_user(name, age) values(?, ?)",[name, age], (err, doc) => {
+            if(err) console.log(err)
+            res.json(true)
+        })
+        conn.releae()
+    })
+
+})
+//conn 이 아니여도 됨
+
 const port = 3030
 app.listen(port, () => console.log(`SERVER ON PORT : ${port}`))
 
