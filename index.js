406880dbda4fca548be1e9f068bfe79603a15f47
Hyerim03 <chhari0708@naver.com>
2022-10-04 20:31:39 +0900

    add : project setting
    DB set
    parser set

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const app = express()
+const pool = require("./config/database")
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 const port = 3030
 app.listen(port, () => console.log(`SERVER ON PORT : ${port}`))
+
+//npm install mysql
